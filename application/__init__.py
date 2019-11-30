aa5bd6e1291a6b7f73a64cb77d7c76ce84d59f32
Taen <taen.heizo8@gmail.com>
2019-11-29 22:46:52 -0500

    __init__.py create app function:

---
+++ application/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+
+from config import configObject
+
+
+def create_app():
+    # instantiate the app
+    app = Flask(__name__, template_folder="templates", static_folder="static")
+    app.config.from_object(configObject)
+
+    # enable CORS
+    CORS(app, resources={r'/*': {'origins': '*'}})
+
+    # with app.app_context():
+    #     app.register_blueprint(blueprint)
+
+    @app.route('/')
+    def top_page():
+        return render_template("index.html")
+
+    # init_db()
+
+    return app
