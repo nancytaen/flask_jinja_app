e90f2efd2a6888edc31a7b47121eb657ad78c28d
Taen <taen.heizo8@gmail.com>
2020-01-07 20:08:08 -0500

    organized application directory

--- application/model/model.py
+++ application/model/model.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from application.model.base import Base
+from model.base import Base
 from config import configObject
 
 
