4b485b8cd675605cb047170bb8c8025d567672b1
Taen <taen.heizo8@gmail.com>
2020-04-12 10:15:15 -0400

    added alembic and modified model file

--- application/model/model.py
+++ application/model/model.py
@@ -1,22 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 
-from application.model.base import Base
+from application.model.base import Base, Session
 from config import configObject
-
-
-try:
-    engine = create_engine(configObject.DATABASE_URI, convert_unicode=True, echo=True)
-    db_session = scoped_session(sessionmaker(autocomit=False, autoflush=False, bind=engine))
-    Base.query = db_session.query_property()
-
-except Exception as e:
-    print("Database Connection Error")
-    print(e)
 
 
 def init_db():
     try:
-        Base.metadata.create_all(bind=engine)
-    except Exception as ex:
-        print(ex)
+        engine = create_engine(configObject.DATABASE_URI, convert_unicode=True, echo=False)
+        Session.configure(bind=engine)
+        Base.query = Session.query_property()
+
+    except Exception as e:
+        print("Database Connection Error")
+        print(e)
